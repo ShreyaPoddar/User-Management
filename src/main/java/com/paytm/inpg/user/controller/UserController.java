019735d741074d0fe25c1148321035eb64525d50
ShreyaPoddar <shreyapoddar02@gmail.com>
2021-01-19 16:07:18 +0530

    Validation in delete method

--- src/main/java/com/paytm/inpg/user/controller/UserController.java
+++ src/main/java/com/paytm/inpg/user/controller/UserController.java
@@ -13,52 +13,57 @@ public class UserController {
     @Autowired
     private UserService service;
 
-  //Post method
+    //Post method
     @PostMapping("/addUser")
-    public String addUser(@RequestBody User user)
-    {
-        List<User> emails=service.findByEmailid(user.getEmailid());
+    public String addUser(@RequestBody User user) {
+        List<User> emails = service.findByEmailid(user.getEmailid());
         List<User> usernames = service.findByUsername(user.getUsername());
         List<User> mobile_numbers = service.findByMobilenumber(user.getMobilenumber());
-        if(!emails.isEmpty())
+        if (!emails.isEmpty())
             return "User with same emailID already exists";
-        else if(!usernames.isEmpty())
+        else if (!usernames.isEmpty())
             return "User with same username already exists";
-        else if(!mobile_numbers.isEmpty())
+        else if (!mobile_numbers.isEmpty())
             return "User with same mobile number already exists";
         service.saveUser(user);
         return "User's data successfully saved";
     }
+
     //Get method
     @GetMapping("/users")
-    public List<User> findAllUsers()
-    {
+    public List<User> findAllUsers() {
         return service.getUser();
     }
+
     @GetMapping("/user/{userid}")
-    public User findUserByid(@PathVariable int userid)
-    {
+    public User findUserByid(@PathVariable int userid) {
         return service.getUserByID(userid);
     }
 
     //update method
     @PutMapping("/update/{userid}")
-    public String updateUser(@RequestBody User user,@PathVariable(value = "userid")int id) {
-        if (service.getUserByID(id) == null)
-            return "User doesn't exist";
-        else {
+    public String updateUser(@RequestBody User user, @PathVariable(value = "userid") int id) {
+        if (service.getUserByID(id) != null) {
             service.updateUser(user);
             return "User data updated";
+        } else {
+            return "User doesn't exist";
+
         }
     }
 
     //delete method
     @DeleteMapping("/delete/{userid}")
-    public String deleteUser(@PathVariable int userid)
-    {
+    public String deleteUser(@PathVariable int userid) {
+        if (service.getUserByID(userid) != null) {
+            service.deleteUser(userid);
+            return "User data deleted";
+        } else {
+            return "User doesn't exist";
+
+        }
 
 
-        return service.deleteUser(userid);
     }
 }
 
