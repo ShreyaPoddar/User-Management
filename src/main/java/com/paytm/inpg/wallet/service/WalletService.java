e039a967bedbba18aaddb89365817d3a68998656
ShreyaPoddar <shreyapoddar02@gmail.com>
2021-01-20 13:39:34 +0530

    Wallet Service class having User and wallet repository

--- src/main/java/com/paytm/inpg/wallet/service/WalletService.java
+++ src/main/java/com/paytm/inpg/wallet/service/WalletService.java
@@ -1,4 +1,15 @@
 package com.paytm.inpg.wallet.service;
 
+
+import com.paytm.inpg.user.repository.UserRepository;
+import com.paytm.inpg.wallet.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WalletService {
+
+    @Autowired
+    private UserRepository userrepository;
+    private WalletRepository walletrepository;
 }
