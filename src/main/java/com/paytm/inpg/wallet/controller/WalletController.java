9f1b01f43c95500c4a6796c0aa614e94925ee7cc
ShreyaPoddar <shreyapoddar02@gmail.com>
2021-01-20 15:58:36 +0530

    Comments in the wallet controller post method

--- src/main/java/com/paytm/inpg/wallet/controller/WalletController.java
+++ src/main/java/com/paytm/inpg/wallet/controller/WalletController.java
@@ -21,12 +21,17 @@ public class WalletController {
     //Post method
     @PostMapping("/addUserWallet/{phonenumber}")
     public String addUser(@RequestBody Wallet wallet, @PathVariable(value = "phonenumber") String mobilenumber) {
+        //finds the user with same mobile number as given in the wallet input
         List<User> mobile_numbers = service.findByMobilenumber(wallet.getPhonenumber());
+        //finds the wallet with same mobile number as given in the wallet input
         List<Wallet> phone_numbers = service.findByPhonenumber(wallet.getPhonenumber());
+        //Checks if the list with the user having same mobile number exists in the user table or not
         if (mobile_numbers.isEmpty())
             return "User data doesn't exist in user table";
+        //if the user wallet is already created
         else if (!phone_numbers.isEmpty())
             return "User with same mobile number already exists";
+        //if the user data exists in the user table and wallet is not created then creating user's wallet
         else {
             service.createWallet(wallet);
             return "User's wallet is successfully created";
